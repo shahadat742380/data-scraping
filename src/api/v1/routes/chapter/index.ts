ac1d9a31b5acf8fa4275553fb5257c6b400f9a28
shahadatpeacock <shahadat.peacockindia@gmail.com>
2024-11-04 13:43:50 +0600

    push last code

--- src/api/v1/routes/chapter/index.ts
+++ src/api/v1/routes/chapter/index.ts
@@ -1,10 +1,31 @@
 import { Hono } from "hono";
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+interface Chapter {
+  title: string;
+  url: string;
+}
 
 const chapter = new Hono();
 
-// auth routes
-chapter.get("/", (c) => {
-  return c.text("The Hono Server is running...!");
+chapter.post("/", async (c) => {
+  const response = await axios.get("https://vedabase.io/en/library/bg/");
+  const $ = cheerio.load(response.data);
+  
+  const chapters: Chapter[] = [];
+  
+  $(".r-chapter a").each((index, element) => {
+    const chapterTitle = $(element).text().trim();
+    const chapterUrl = $(element).attr("href");
+    
+    if (chapterUrl) {
+      chapters.push({ title: chapterTitle, url: chapterUrl });
+    }
+  });
+  
+  console.log(chapters);
+  return c.json(chapters);
 });
 
 export default chapter;
